ef07799a79f69fb9a7b411fb7d687b92b15a51d2
Joab Pereira <143361148+JoabPereira@users.noreply.github.com>
2024-04-30 17:56:14 -0300

    Delete ListaDeExercicio.JS directory

--- ListaDeExercicio.JS/Questao05/script.js
+++
@@ -1,4 +0,0 @@
-let raio = parseFloat(prompt("Raio do circulo: "));
-
-console.log("Comprimento da circunferencia: ", 2 * Math.PI * raio);
-console.log("Area da circunferencia: ", Math.PI * Math.pow(raio, 2))
